cf20868240f571682bc8bd7572d42f53fdb0472a
zoee2 <lek1123@naver.com>
2024-02-02 15:30:17 +0900

    install axios

---
+++ vue3-form/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <form action="" @submit.prevent="submitForm">
+    <div>
+      <label for="userId">ID : </label>
+      <input id="userId" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">PW : </label>
+      <input id="password" type="text" v-model="password">
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      const data = {
+        username: this.username,
+        password: this.password,
+      }
+      axios.post('https://jsonplaceholder.typicode.com/users/', data)
+        .then(response => {
+          console.log('response')
+        });
+      // console.log('제출됨')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
